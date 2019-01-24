aab02430438ef1a29b162032e6c384bf1602c6bd
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-01-24 10:42:04 -0200

    Add blank Xcode project

---
+++ On The Map/On The Map/Supporting Files/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  On The Map
+//
+//  Created by Tiago Maia Lopes on 24/01/19.
+//  Copyright © 2019 Tiago Maia Lopes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: Properties
+
+    var window: UIWindow?
+
+    // MARK: App Life Cycle
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
